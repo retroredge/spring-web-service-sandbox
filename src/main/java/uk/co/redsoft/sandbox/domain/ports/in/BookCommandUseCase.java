0253da6fd31fc0366c811ca5011ebe37bdc2f603
Nick Redshaw <nick@redshaw>
2026-05-28 08:43:29 +0100

    Delete unused code

--- src/main/java/uk/co/redsoft/sandbox/domain/ports/in/BookCommandUseCase.java
+++
@@ -1,9 +0,0 @@
-package uk.co.redsoft.sandbox.domain.ports.in;
-
-import uk.co.redsoft.sandbox.domain.model.Book;
-import uk.co.redsoft.sandbox.domain.model.CreateBookCommand;
-
-public interface BookCommandUseCase {
-    Book create(CreateBookCommand command);
-    void importBook(CreateBookCommand command);
-}
